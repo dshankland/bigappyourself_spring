985d7b98597a1beb525dccd18657513d1f7ead34
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-06-22 12:44:45 +0100

    tweaks gitignore and SmsSender

--- src/main/java/com/codeclan/example/BigAppYourself/SMS/SmsSender.java
+++ src/main/java/com/codeclan/example/BigAppYourself/SMS/SmsSender.java
@@ -7,8 +7,8 @@ import com.twilio.type.PhoneNumber;
 
 public class SmsSender {
 
-    public static final String ACCOUNT_SID = "AC934c54990a52597e0980a242b7bcdd4a";
-    public static final String AUTH_TOKEN = "y9dac0ab9c1a4b05ccffa22b4fd5812fd";
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
