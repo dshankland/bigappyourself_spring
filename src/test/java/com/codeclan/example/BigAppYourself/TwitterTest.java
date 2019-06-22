116b8afd87edbbf14b4938c18143c5d39a6df050
JMMoir <jmmoir@outlook.com>
2019-06-22 13:08:14 +0100

    tweet a randomly generated compliment

--- src/test/java/com/codeclan/example/BigAppYourself/TwitterTest.java
+++ src/test/java/com/codeclan/example/BigAppYourself/TwitterTest.java
@@ -1,24 +1,48 @@
 package com.codeclan.example.BigAppYourself;
 
+import com.codeclan.example.BigAppYourself.repositories.UserRepository;
 import com.codeclan.example.BigAppYourself.twitter.TwitterApp;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import twitter4j.TwitterException;
+import com.codeclan.example.BigAppYourself.models.Keyword;
+import com.codeclan.example.BigAppYourself.models.User;
 
 import static org.junit.Assert.assertEquals;
 
 public class TwitterTest {
 
-    String tweet = "Your shoulder blades are amazing!";
+    @Autowired
+    UserRepository userRepository;
 
-    @Test
-    public void givenText_updateStatus() throws TwitterException {
-        String text = TwitterApp.createTweet(tweet);
-        assertEquals(tweet, text);
+    private User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User("John", "Moir", "john@4bigappyourself.com", "password" );
     }
+
+//    String tweet = "Your git merge is amazing!";
+//
+//    @Test
+//    public void givenText_updateStatus() throws TwitterException {
+//        String text = TwitterApp.createTweet(tweet);
+//        assertEquals(tweet, text);
+//    }
 
     @Test
     public void canSendDirectMessage() throws TwitterException{
         TwitterApp.sendDirectMessage("@hughdjarvis", "Tweet Tweet");
         TwitterApp.sendDirectMessage("@darrenshankland", "Tweet Tweet");
     }
+
+    @Test
+    public void canGenerateComplimentAndTweet() throws TwitterException{
+        user.generateCompliment();
+        String randomComp = user.getCompliment();
+        TwitterApp.createTweet(randomComp);
+    }
+
+
 }
