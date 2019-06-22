f7b9534dfb2a73b24f9ab503c95162462006b4d5
JMMoir <jmmoir@outlook.com>
2019-06-22 12:26:26 +0100

    .gitignore update

--- src/test/java/com/codeclan/example/BigAppYourself/BigAppYourselfApplicationTests.java
+++ src/test/java/com/codeclan/example/BigAppYourself/BigAppYourselfApplicationTests.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BigAppYourselfTwitterAppTests {
+public class BigAppYourselfApplicationTests {
 
 	private LocalDate user1Birthday;
 	private LocalDate user2Birthday;
