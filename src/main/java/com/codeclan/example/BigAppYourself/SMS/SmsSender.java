88f619a66ce4a60a98ba55321e14a93a4c358870
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-06-24 10:48:12 +0100

    abandons env variables

--- src/main/java/com/codeclan/example/BigAppYourself/SMS/SmsSender.java
+++ src/main/java/com/codeclan/example/BigAppYourself/SMS/SmsSender.java
@@ -1,21 +1,24 @@
 package com.codeclan.example.BigAppYourself.SMS;
 
+import com.codeclan.example.BigAppYourself.models.User;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-
+@Component
+@Service
 public class SmsSender {
 
     public static final String ACCOUNT_SID = System.getenv("TWILIO_SID");
     public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
-    public static void main(String[] args) {
+    public void sendSMS(String compliment, String number)  {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message.creator(new PhoneNumber("+441253530348"),
-                new PhoneNumber("+447813684853"),
-                "Your coding skills are no bad").create();
+                new PhoneNumber(number), compliment).create();
 
        // System.out.println(message.getSid());
 
