b08748ce08973392025fc43c85da032fa8658bad
Darren Shankland <ds@4pq.co.uk>
2019-06-24 10:52:54 +0100

    add compliment route

--- src/main/java/com/codeclan/example/BigAppYourself/controllers/UserController.java
+++ src/main/java/com/codeclan/example/BigAppYourself/controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.codeclan.example.BigAppYourself.controllers;
 
+import com.codeclan.example.BigAppYourself.models.User;
 import com.codeclan.example.BigAppYourself.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -11,4 +18,13 @@ public class UserController {
 
     @Autowired
     UserRepository userRepository;
+
+
+    @GetMapping(value = "/compliment/{id}")
+    public User updateUserCompliment(@PathVariable(value = "id") Long id) {
+        User existingUser = userRepository.findById(id).get();
+        existingUser.generateCompliment();
+        userRepository.save(existingUser);
+        return existingUser;
+    }
 }
--- src/main/java/com/codeclan/example/BigAppYourself/repositories/UserRepository.java
+++ src/main/java/com/codeclan/example/BigAppYourself/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.codeclan.example.BigAppYourself.repositories;
 
 import com.codeclan.example.BigAppYourself.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
 }
